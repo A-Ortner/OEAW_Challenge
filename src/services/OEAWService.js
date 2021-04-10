73c7703b80adf0b528792cabeb51c4f299153b85
Andrea <e11809650@student.tuwien.ac.at>
2021-04-10 19:43:56 +0200

    [service] added documentation and getPage() to load a new page for pagination

--- src/services/OEAWService.js
+++ src/services/OEAWService.js
@@ -1,23 +1,45 @@
 const OEAW_BASE_URL = 'https://mmp.acdh-dev.oeaw.ac.at/api/';
 
-//stelle/?zitat=gentis
-
+/**
+ * Fetches text-passages that match the keyword(s) in the query
+ * @param query: keyword(s) to be searched for
+ * @param limit: number of datarows returned
+ * @return a promise/list of matching datarows; including refences to the next/previous page (if paging is used)
+ */
 export async function getPassages(query = '', limit = 20) {
-    console.log('getPassages()');
+    console.log('getPassages()', query);
     const q = query.trim() == '' ? '' : query.trim();
     console.log(OEAW_BASE_URL + 'stelle/?limit=' + limit.toString() + '&offset=20' + '&zitat=' + q);
-    const res = await fetch(OEAW_BASE_URL + 'stelle/?limit=' + limit.toString()+ '&offset=20' + '&zitat=' + q);
+    const res = await fetch(OEAW_BASE_URL + 'stelle/?limit=' + limit.toString() + '&offset=20' + '&zitat=' + q);
     if (!res.ok) {
         return []
     }
 
-    let resjson = res.json();
-    return resjson;
+    return res.json();
+
 }
 
+/**
+ * Fetches a specific page for pagination via API call
+ * @param request: API call to be made
+ * @return a promise/list of datarows; including refences to the next/previous page (if paging is used)
+ */
+export async function getPage(request) {
+    console.log("getPage()", request);
+    const res = await fetch(request);
+    if (!res.ok) {
+        return []
+    }
+    return res.json();
+
+}
+
+/**
+ * Fetches a specific author via API call
+ * @param request API call including the author´s id
+ * @return promise/specified author
+ */
 export async function getAuthor(request = '') {
-    console.log('getAuthor()');
-    console.log(request);
     const res = await fetch(request);
     if (!res.ok) {
         return []
@@ -25,11 +47,16 @@ export async function getAuthor(request = '') {
     return res.json();
 }
 
+/**
+ * Fetches specific passage from DB
+ * @param id: id of the passage to be fetched
+ * @return promise/specified passage
+ */
 export async function getPassageById(id = null) {
-    console.log('getPassageByLegacyId()');
-    if(id == null) return;
-    console.log(OEAW_BASE_URL + 'stelle/'+id.toString())
-    const res = await fetch(OEAW_BASE_URL + 'stelle/'+ id.toString());
+    console.log('getPassageByLegacyId()', id);
+    if (id == null) return;
+    console.log(OEAW_BASE_URL + 'stelle/' + id.toString())
+    const res = await fetch(OEAW_BASE_URL + 'stelle/' + id.toString());
     if (!res.ok) {
         return null;
     }
